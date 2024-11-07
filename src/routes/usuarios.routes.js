0bd03f0c28d30b7339cd97748726d2cad5a39551
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-07 15:41:45 -0300

    feat: create method get

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -12,14 +12,12 @@ let treinamento = [
   },
 ];
 
-usuariosRoutes.get("/", (req, res) => {
-  return res.status(200).json({
-    message:
-      usuarios.length == 0
-        ? "Não há usuários cadastrados"
-        : `Total de usuários: ${usuarios.length}`,
-    usuarios,
-  });
-});
 
-export default usuariosRoutes;
+treinamentoRoutes.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Não há treinamentos cadastrados!",
+    treinamento,
+  });
+}); 
+
+export default treinamentoRoutes;
